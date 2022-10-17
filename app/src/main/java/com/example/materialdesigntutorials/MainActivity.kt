9163ea4f94e63c3004919d8e55defd305de3cc88
tuana <https://github.com/TuanAnhmvp/Android-Studio>
2022-10-17 20:57:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/materialdesigntutorials/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.materialdesigntutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.prominent_top_app_bar)
+    }
+}
